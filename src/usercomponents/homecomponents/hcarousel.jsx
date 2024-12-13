5c852bc09993c7ecb27fcc1c55d80e0136b3b5dc
sankarsantosh99 <ssankarkumar1999@gmail.com>
2024-12-13 18:39:54 +0530

    Carousel

---
+++ src/usercomponents/homecomponents/hcarousel.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./hcarousel.css";
+
+
+
+function Hcarousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slides = ["/assets/greenleaves.jpg","/assets/greenleaves.jpg","/assets/greenleaves.jpg"];
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => prevIndex === 0 ? slides.length - 1 : prevIndex - 1);
+  };
+
+  return (
+    <>
+      <div className="carousel">
+        <div
+          className="carousel-slides"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+          }}
+        >
+          {slides.map((src, index) => (
+            <div className="slide">
+              <img src={src} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+
+        <button className="prev" onClick={handlePrev}>
+          ❮
+        </button>
+        <button className="next" onClick={handleNext}>
+          ❯
+        </button>
+
+        <div className="indicators">
+          {slides.map((_, index) => (
+            <span
+              key={index}
+              className={`indicator ${index === currentIndex ? "active" : ""}`}
+              onClick={() => setCurrentIndex(index)}
+            ></span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Hcarousel;
--- src/userpages/homepage/homepage.jsx
+++ src/userpages/homepage/homepage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HomeHeader from '../../usercomponents/homecomponents/homeheader';
 import "./homepage.css"
-import Hcarousal from '../../usercomponents/homecomponents/hcarousal';
+import Hcarousel from '../../usercomponents/homecomponents/hcarousel';
 
 function HomePage ()  {
   return (
     <>
     <div>
         <HomeHeader />
-        < Hcarousal />
+        <Hcarousel />
         
     </div>
     </>
