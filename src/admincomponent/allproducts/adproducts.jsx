91a90bb6cc91dcbd6b7cac3fa6c4047f321f2391
sankarsantosh99 <ssankarkumar1999@gmail.com>
2024-12-27 12:32:01 +0530

    push for system

--- src/admincomponent/allproducts/adproducts.jsx
+++ src/admincomponent/allproducts/adproducts.jsx
@@ -4,28 +4,31 @@ import "./adproducts.css";
 function AdProducts() {
   return (
     <>
-      
-        <div className="prcontainer">
+      <div className="prcontainer">
+        <div className="pritem">
+          <div className="cardtitle">
+            <img src="favicon.ico" className="primg" />
 
-          <div className="pritem">
-           
-            <div className="cardtitle">
-              <img src="favicon.ico" className="primg" />
-              <div className="product">
-                <span className="prname">Lorem Ipsum</span>
-                <span className="prtype">Cracker</span>
-                <span className="prprice">₹1000</span>
-              </div>
+            <div className="product">
+              <span className="prname">Lorem Ipsum</span>
+              <span className="prtype">Cracker</span>
+              <span className="prprice">₹1000</span>
             </div>
           </div>
 
-          <div className="pritem">2</div>
-
-          <div className="pritem">3</div>
-
-          <div className="pritem">4</div>
+          <div className="prsummary">
+            <h5>Summary</h5>
+            <p className="prpara">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ab nisi nobis exercitationem animi vel.</p>
+          </div>
+          <div className=""></div>
         </div>
-      
+
+        <div className="pritem">2</div>
+
+        <div className="pritem">3</div>
+
+        <div className="pritem">4</div>
+      </div>
     </>
   );
 }
